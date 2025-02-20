165361348f589f086a09bc75d302c1f26aeaff7c
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-02-20 18:53:41 +0100

    digital ocean was added

--- next.config.js
+++ next.config.js
@@ -36,6 +36,11 @@ const nextConfig = {
         protocol: "https",
         hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
       },
+      {
+        protocol: "https",
+        hostname: "medusajs-server.fra1.cdn.digitaloceanspaces.com",
+      },
+
     ],
   },
 }
