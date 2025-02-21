1fcd548212b66a5692ccbe60374cf715b1004dac
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-02-21 11:33:05 +0100

    search was added

--- src/app/[countryCode]/(main)/page.tsx
+++ src/app/[countryCode]/(main)/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from "next"
 
-import FeaturedProducts from "@modules/store/components/featured-products"
+import FeaturedProducts from "@modules/home/components/featured-products"
 import Hero from "@modules/home/components/hero"
 import SearchBar from "@modules/home/components/hero/search";
 
