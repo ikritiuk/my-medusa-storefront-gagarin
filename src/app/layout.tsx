43226ca4736d6ab3806cb07324dac58c7244e61b
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-02-21 12:16:03 +0100

    search was added fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,27 @@
-import { getBaseURL } from "@lib/util/env"
-import { Metadata } from "next"
-import "styles/globals.css"
+"use client";
+
+import { getBaseURL } from "@lib/util/env";
+import { Metadata } from "next";
+import { MedusaProvider } from "medusa-react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "styles/globals.css";
+
+const queryClient = new QueryClient();
 
 export const metadata: Metadata = {
   metadataBase: new URL(getBaseURL()),
-}
+};
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="light">
-      <body>
+    <body>
+    <QueryClientProvider client={queryClient}>
+      <MedusaProvider baseUrl={process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL || "http://localhost:9000"}>
         <main className="relative">{props.children}</main>
-      </body>
+      </MedusaProvider>
+    </QueryClientProvider>
+    </body>
     </html>
-  )
+  );
 }
