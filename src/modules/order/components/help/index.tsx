251ba2975b5eed6eb10a0e7d6d1c4fe002045568
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-03-04 23:46:32 +0100

    i18n fixes

--- src/modules/order/components/help/index.tsx
+++ src/modules/order/components/help/index.tsx
@@ -5,16 +5,11 @@ import React from "react"
 const Help = () => {
   return (
     <div className="mt-6">
-      <Heading className="text-base-semi">Need help?</Heading>
+      <Heading className="text-base-semi">Нужна помощь?</Heading>
       <div className="text-base-regular my-2">
         <ul className="gap-y-2 flex flex-col">
           <li>
-            <LocalizedClientLink href="/contact">Contact</LocalizedClientLink>
-          </li>
-          <li>
-            <LocalizedClientLink href="/contact">
-              Returns & Exchanges
-            </LocalizedClientLink>
+            <LocalizedClientLink href="/contact">Техподдержка</LocalizedClientLink>
           </li>
         </ul>
       </div>
