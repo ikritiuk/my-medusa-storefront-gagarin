10c61729a6fc5f153963cc058675f8cb19bbf78d
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-03-14 14:23:28 +0100

    homepage refactoring mobile

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -241,13 +241,13 @@ const Hero = () => {
           {/* Brand Images */}
           <div className="flex flex-col items-center gap-4">
             {/* Brand Images */}
-            <div className="relative w-full text-center overflow-hidden rounded shadow-lg h-[220px]">
+            <div className="relative w-full text-center overflow-hidden rounded shadow-lg h-[250px] sm:h-[280px] md:h-1/2 lg:h-full">
               <div className="absolute top-2 left-2 bg-black bg-opacity-50 text-white text-lg px-2 py-1 rounded z-10">
                 DJI Дроны
               </div>
               <Link href="/collections/dji-drones">
                 <img
-                  src="https://medusajs-server.fra1.cdn.digitaloceanspaces.com/dji-microphones.avif"
+                  src="https://medusajs-server.fra1.cdn.digitaloceanspaces.com/dji-drones.avif"
                   alt="DJI Дроны"
                   className="w-full h-full object-cover rounded shadow-lg transition-transform duration-500 hover:scale-110"
                 />
@@ -285,7 +285,7 @@ const Hero = () => {
           </div>
 
           <div className="flex flex-col items-center gap-4 ">
-            <div className="relative w-full text-center overflow-hidden rounded shadow-lg h-[220px]">
+            <div className="relative w-full text-center overflow-hidden rounded shadow-lg h-[250px] sm:h-[280px] md:h-1/2 lg:h-full">
               <div className="absolute top-2 left-2 bg-black bg-opacity-50 text-white text-lg px-2 py-1 rounded z-10">
                 DJI Микрофоны
               </div>
