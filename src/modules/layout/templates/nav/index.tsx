b79d2cf1a4f87db9a18b95faef21b7f77d7acb69
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-03-24 20:22:07 +0100

    bottom bar was added for mobile

--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -82,30 +82,6 @@ export default async function Nav() {
 
       {/* Mobile Bottom Bar */}
       <div className="fixed bottom-0 inset-x-0 z-50 bg-white border-t border-gray-200 flex justify-around items-center py-2 md:hidden">
-        {/* Hamburger menu icon */}
-        <button
-          onClick={() => {
-            // Optional: Trigger side menu toggle if you have mobile logic
-          }}
-          className="flex flex-col items-center text-xs text-gray-700 hover:text-black"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 mb-1"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={2}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
-          Меню
-        </button>
-
         <a
           href="/"
           className="flex flex-col items-center text-xs text-gray-700 hover:text-black"
