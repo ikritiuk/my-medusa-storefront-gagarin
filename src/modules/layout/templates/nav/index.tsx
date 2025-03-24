38fd22d0ea6829d7c2cf4024227b17b7cd230c87
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-03-24 15:31:43 +0100

    logo was added

--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -20,14 +20,11 @@ export default async function Nav() {
             </div>
           </div>
 
-          <div className="flex items-center h-full">
-            <LocalizedClientLink
-              href="/"
-              className="txt-compact-xlarge-plus hover:text-ui-fg-base uppercase"
-              data-testid="nav-store-link"
-            >
-              Gagarin shop
-            </LocalizedClientLink>
+          <div>
+            <a
+              href="/">
+              <img className="w-full h-auto md:w-1/1 mx-auto" src="/logo.svg" alt="Logo"/>
+            </a>
           </div>
           <Search />
           <div className="flex items-center gap-x-6 h-full flex-1 basis-0 justify-end">
