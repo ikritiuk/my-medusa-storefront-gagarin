712d01a6e56fad4cfb80acb722296cdfe7ed01d8
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-03-24 16:55:21 +0100

    logo was added

--- src/modules/layout/components/side-menu/index.tsx
+++ src/modules/layout/components/side-menu/index.tsx
@@ -30,7 +30,17 @@ const SideMenu = ({ regions }: { regions: HttpTypes.StoreRegion[] | null }) => {
                   data-testid="nav-menu-button"
                   className="relative h-full flex items-center transition-all ease-out duration-200 focus:outline-none hover:text-ui-fg-base"
                 >
-                 Меню
+                  {/* Replace text with hamburger icon */}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-8 w-8"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+                  </svg>
                 </Popover.Button>
               </div>
 
