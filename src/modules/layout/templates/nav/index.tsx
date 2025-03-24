42cdc78253bbc98dacaedb42c165831c1dfe5f16
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-03-24 17:13:56 +0100

    logo is centered

--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -14,26 +14,19 @@ export default async function Nav() {
     <div className="sticky top-0 inset-x-0 z-50 group">
       <header className="relative h-16 mx-auto border-b duration-200 bg-white border-ui-border-base">
         <nav
-          className="content-container txt-xsmall-plus text-ui-fg-subtle flex items-center justify-between w-full h-full text-small-regular relative">
+          className="content-container txt-xsmall-plus text-ui-fg-subtle flex items-center justify-between w-full h-full text-small-regular">
           <div className="flex-1 basis-0 h-full flex items-center">
             <div className="h-full">
+              <div className="flex items-center justify-center py-4">
+                <a
+                  href="/">
+                  <img className="h-24 w-auto md:h-[200px] lg:h-[220px]" src="/logo.svg" alt="Logo"/>
+                </a>
+              </div>
               <SideMenu regions={regions}/>
             </div>
           </div>
-
-          {/* Centered logo on desktop */}
-          <div className="flex items-center justify-center py-4 md:absolute md:left-1/2 md:-translate-x-1/2">
-            <a href="/">
-              <img
-                className="h-24 w-auto md:h-[200px] lg:h-[220px]"
-                src="/logo.svg"
-                alt="Logo"
-              />
-            </a>
-          </div>
-
-          <Search />
-
+          <Search/>
           <div className="flex items-center gap-x-6 h-full flex-1 basis-0 justify-end">
             <div className="hidden small:flex items-center gap-x-6 h-full">
               <LocalizedClientLink
@@ -55,7 +48,7 @@ export default async function Nav() {
                 </LocalizedClientLink>
               }
             >
-              <CartButton />
+              <CartButton/>
             </Suspense>
           </div>
         </nav>
