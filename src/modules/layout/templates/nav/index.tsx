0445fbf01ab0990342ab6c9e9f39861a24a40814
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-03-24 20:34:32 +0100

    Revert "cart icon refactoring"
    This reverts commit bce28d52

--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -1,5 +1,4 @@
-"use client"
-import { default as React, Suspense, useEffect, useState } from "react"
+import { default as React, Suspense } from "react"
 
 import { listRegions } from "@lib/data/regions"
 import { StoreRegion } from "@medusajs/types"
@@ -7,7 +6,6 @@ import LocalizedClientLink from "@modules/common/components/localized-client-lin
 import CartButton from "@modules/layout/components/cart-button"
 import SideMenu from "@modules/layout/components/side-menu"
 import Search from "@modules/home/components/search"
-import { useCart } from "@medusajs/react" // ✅ Medusa 2.0 hook
 
 export default async function Nav() {
   const regions = await listRegions().then((regions: StoreRegion[]) => regions)
@@ -82,92 +80,72 @@ export default async function Nav() {
         </header>
       </div>
 
-      {/* Mobile Bottom Bar with dynamic cart count */}
-      <MobileBottomBar />
+      {/* Mobile Bottom Bar */}
+      <div className="fixed bottom-0 inset-x-0 z-50 bg-white border-t border-gray-200 flex justify-around items-center py-2 md:hidden">
+        <a
+          href="/"
+          className="flex flex-col items-center text-xs text-gray-700 hover:text-black"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 mb-1"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 9.75L12 4l9 5.75M4.5 10.5v9h15v-9"
+            />
+          </svg>
+          Главная
+        </a>
+
+        <a
+          href="/account"
+          className="flex flex-col items-center text-xs text-gray-700 hover:text-black"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 mb-1"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 12c2.21 0 4-1.79 4-4S14.21 4 12 4s-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4
+              v2h16v-2c0-2.66-5.33-4-8-4z"
+            />
+          </svg>
+          Профиль
+        </a>
+
+        <a
+          href="/cart"
+          className="flex flex-col items-center text-xs text-gray-700 hover:text-black"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 mb-1"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13l-1.3 5.3a1 1 0 001 1.2h12.6a1 1 0 001-1.2L17 13M7 13l-4-8m4 8l4-8"
+            />
+          </svg>
+          Корзина
+        </a>
+      </div>
     </>
-  )
-}
-
-function MobileBottomBar() {
-  const { cart } = useCart()
-  const [cartCount, setCartCount] = useState(0)
-
-  useEffect(() => {
-    if (cart && cart.items) {
-      setCartCount(cart.items.reduce((total, item) => total + item.quantity, 0))
-    }
-  }, [cart])
-
-  return (
-    <div className="fixed bottom-0 inset-x-0 z-50 bg-white border-t border-gray-200 flex justify-around items-center py-2 md:hidden">
-      <a
-        href="/"
-        className="flex flex-col items-center text-xs text-gray-700 hover:text-black"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6 mb-1"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth={2}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M3 9.75L12 4l9 5.75M4.5 10.5v9h15v-9"
-          />
-        </svg>
-        Главная
-      </a>
-
-      <a
-        href="/account"
-        className="flex flex-col items-center text-xs text-gray-700 hover:text-black"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6 mb-1"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth={2}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M12 12c2.21 0 4-1.79 4-4S14.21 4 12 4s-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4
-            v2h16v-2c0-2.66-5.33-4-8-4z"
-          />
-        </svg>
-        Профиль
-      </a>
-
-      <a
-        href="/cart"
-        className="relative flex flex-col items-center text-xs text-gray-700 hover:text-black"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6 mb-1"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth={2}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13l-1.3 5.3a1 1 0 001 1.2h12.6a1 1 0 001-1.2L17 13M7 13l-4-8m4 8l4-8"
-          />
-        </svg>
-        Корзина
-        {cartCount > 0 && (
-          <span className="absolute -top-1 -right-2 bg-red-500 text-white text-xs rounded-full px-1">
-            {cartCount}
-          </span>
-        )}
-      </a>
-    </div>
   )
 }
