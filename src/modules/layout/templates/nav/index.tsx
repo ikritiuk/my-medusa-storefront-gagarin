c35c8f241aace9215e09758c751a09a43a642cf5
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-03-25 15:31:20 +0100

    bar icons was updated. vorking version

--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -18,7 +18,7 @@ export default async function Nav() {
             className="content-container txt-xsmall-plus text-ui-fg-subtle flex items-center justify-between w-full h-full text-small-regular relative"
           >
             <div className="hidden md:block h-full">
-              <SideMenu regions={regions}/>
+              <SideMenu regions={regions} />
             </div>
 
             <div className="hidden md:flex items-center justify-center py-4 mr-4 md:mr-10">
@@ -32,11 +32,11 @@ export default async function Nav() {
             </div>
 
             <div className="hidden md:flex absolute left-1/2 -translate-x-1/2">
-              <Search/>
+              <Search />
             </div>
 
             <div className="flex md:hidden">
-              <Search/>
+              <Search />
             </div>
 
             <div className="flex items-center gap-x-6 h-full flex-1 basis-0 justify-end">
@@ -65,7 +65,7 @@ export default async function Nav() {
                   }
                 >
                   <div className="hidden md:flex">
-                    <CartButton/>
+                    <CartButton />
                   </div>
                 </Suspense>
               </div>
@@ -74,18 +74,26 @@ export default async function Nav() {
         </header>
       </div>
 
-      {/* ✅ Mobile Bottom Bar with bigger cart icon */}
+      {/* ✅ Mobile Bottom Bar with updated home & account icons */}
       <div
-        className="fixed bottom-0 inset-x-0 z-50 bg-white border-t border-gray-200 flex justify-around items-center py-2 md:hidden">
+        className="fixed bottom-0 inset-x-0 z-50 bg-white border-t border-gray-200 flex justify-around items-center py-2 md:hidden"
+      >
         <a
           href="/"
           className="flex flex-col items-center text-xs text-gray-700 hover:text-black"
         >
-          <img
-            src="data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2220%22%20height%3D%2220%22%20fill%3D%22none%22%3E%3Cpath%20fill%3D%22%23D3D4DD%22%20d%3D%22m19.463%208.858-.002-.002L11.302.54A1.829%201.829%200%200%200%2010%200a1.83%201.83%200%200%200-1.302.54L.543%208.851l-.008.008a1.843%201.843%200%200%200%20.003%202.6%201.83%201.83%200%200%200%201.279.54h.325v5.845c0%201.188.967%202.155%202.155%202.155H7.49a.586.586%200%200%200%20.586-.586v-4.707c0-.542.442-.983.984-.983h1.883c.542%200%20.983.44.983.983v4.707c0%20.324.262.586.586.586h3.192a2.157%202.157%200%200%200%202.155-2.155V12h.302a1.83%201.83%200%200%200%201.302-.54%201.844%201.844%200%200%200%200-2.602Z%22%2F%3E%3C%2Fsvg%3E"
-            alt="Home"
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            fill="none"
             className="w-6 h-6 mb-1"
-          />
+          >
+            <path
+              fill="#D3D4DD"
+              d="m19.463 8.858-.002-.002L11.302.54A1.829 1.829 0 0 0 10 0a1.83 1.83 0 0 0-1.302.54L.543 8.851l-.008.008a1.843 1.843 0 0 0 .003 2.6 1.83 1.83 0 0 0 1.279.54h.325v5.845c0 1.188.967 2.155 2.155 2.155H7.49a.586.586 0 0 0 .586-.586v-4.707c0-.542.442-.983.984-.983h1.883c.542 0 .983.44.983.983v4.707c0 .324.262.586.586.586h3.192a2.157 2.157 0 0 0 2.155-2.155V12h.302a1.83 1.83 0 0 0 1.302-.54 1.844 1.844 0 0 0 0-2.602Z"
+            />
+          </svg>
           Главная
         </a>
 
@@ -93,11 +101,20 @@ export default async function Nav() {
           href="/account"
           className="flex flex-col items-center text-xs text-gray-700 hover:text-black"
         >
-          <img
-            src="data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2220%22%20height%3D%2220%22%20fill%3D%22none%22%3E%3Cpath%20fill%3D%22%23C8C8D1%22%20fill-rule%3D%22evenodd%22%20d%3D%22M14.759%204.789c0%202.77-2.195%204.918-4.753%204.918-2.558%200-4.752-2.148-4.752-4.895C5.241%202.101%207.461%200%2010.006%200a4.763%204.763%200%200%201%204.753%204.789ZM0%2018.498C0%2019.484.677%2020%202.57%2020h14.86c1.893%200%202.57-.516%202.57-1.502%200-2.864-3.85-6.808-9.994-6.808C3.85%2011.69%200%2015.634%200%2018.498Z%22%20clip-rule%3D%22evenodd%22%2F%3E%3C%2Fsvg%3E"
-            alt="Account"
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            fill="none"
             className="w-6 h-6 mb-1"
-          />
+          >
+            <path
+              fill="#C8C8D1"
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M14.759 4.789c0 2.77-2.195 4.918-4.753 4.918-2.558 0-4.752-2.148-4.752-4.895C5.241 2.101 7.461 0 10.006 0a4.763 4.763 0 0 1 4.753 4.789ZM0 18.498C0 19.484.677 20 2.57 20h14.86c1.893 0 2.57-.516 2.57-1.502 0-2.864-3.85-6.808-9.994-6.808C3.85 11.69 0 15.634 0 18.498Z"
+            />
+          </svg>
           Профиль
         </a>
 
@@ -140,7 +157,6 @@ export default async function Nav() {
             <CartButton />
           </a>
         </Suspense>
-
       </div>
     </>
   )
