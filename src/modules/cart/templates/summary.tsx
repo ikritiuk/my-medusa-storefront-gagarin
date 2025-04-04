63c7cb85ea11c27228e17f4bf1572a1547a27a33
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-04-04 18:57:11 +0200

    summary

--- src/modules/cart/templates/summary.tsx
+++ src/modules/cart/templates/summary.tsx
@@ -31,7 +31,7 @@ const Summary = ({ cart }: SummaryProps) => {
   return (
     <div className="flex flex-col gap-y-4">
       <Heading level="h2" className="text-[2rem] leading-[2.75rem]">
-        Summary
+        Итог
       </Heading>
       <DiscountCode cart={cart} />
       <Divider />
