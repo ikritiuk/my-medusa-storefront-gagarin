ff875e45fcc7bc9b021422b0ece57f059d655ff7
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-04-08 12:18:24 +0200

    add to cart mobile refactoring

--- src/modules/products/components/product-actions/mobile-actions.tsx
+++ src/modules/products/components/product-actions/mobile-actions.tsx
@@ -65,10 +65,10 @@ const MobileActions: React.FC<MobileActionsProps> = ({
           leaveTo="opacity-0"
         >
           <div
-            className="bg-white flex flex-col gap-y-3 justify-center items-center text-large-regular p-10 w-full border-t border-gray-200 rounded-t-xl shadow-lg"
+            className="bg-white flex flex-col gap-y-3 justify-center items-center text-large-regular p-8 w-full border-t border-gray-200 rounded-t-xl shadow-lg"
             data-testid="mobile-actions"
           >
-            <div className="flex items-center gap-x-2">
+            <div className="flex items-center gap-x-2 pb-4">
               <span data-testid="mobile-title">{product.title}</span>
               <span>—</span>
               {selectedPrice ? (
