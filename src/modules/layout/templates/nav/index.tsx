7d359d347239794d9ac85484b2d672632feccf8d
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-04-08 13:10:43 +0200

    logo mobile resize

--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -25,7 +25,7 @@ export default async function Nav() {
                 <img
                   src="/logo.svg"
                   alt="Logo"
-                  className="h-32 w-33"
+                  className="h-[7.5rem] w-auto"
                 />
               </a>
             </div>
